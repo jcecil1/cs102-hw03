b0c74b375abad83db4f1d43f71a136a7acbd3939
jcecil1 <54964147+jcecil1@users.noreply.github.com>
2019-12-18 15:28:18 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import sys
-
+from statistics import mean
 
 def main():
     assert len(sys.argv) > 1, "No input file path specified."
 
     input_file_path = sys.argv[1]
-    print(f"Processing input file: {input_file_path}")
+    print("Processing input file: {input_file_path}")
 
-    # TODO: Fill in the actual logic here!
+    with open(input_file_path) as f:
+        for line in f:
+            avgset = line[:-1].split(',')
+            avgset = set(map(float, avgset))
+            avg = mean(avgset)
+            print(avg)
 
-
-if __name__ == "__main__":
+if __name__== "__main__":
     main()
